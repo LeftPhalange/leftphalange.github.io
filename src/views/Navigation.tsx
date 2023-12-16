8071d3a46ab4b4875184e02863be67589fdcb076
Ethan Bovard <ethanbovard@hotmail.com>
2023-12-15 23:03:14 -0500

    Improved responsiveness for navigation bar

--- src/views/Navigation.tsx
+++ src/views/Navigation.tsx
@@ -1,7 +1,7 @@
 export default function Navigation() {
     return (
-        <header className="flex flex-row space-x-2 p-4 border-b border-zinc-800 sm:px-16">
-            <div className="flex flex-row mr-auto my-auto">
+        <header className="flex flex-row space-x-2 border-b border-zinc-800">
+            <div className="flex flex-row mr-auto my-auto lg:px-64 px-8 py-4">
                 <a
                     href="/"
                     className="my-auto mx-4"
